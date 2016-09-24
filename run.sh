68778d46af2389b390483813e2ca82425a7c1f85
mlesko <mlesko@paypal.com>
2016-09-24 19:55:59 -0400

    first commit

---
+++ run.sh
@@ -0,0 +1,9 @@
+python search.py breadth node_file.txt edge_file.txt heuristics_file.txt  A G output_breadth.txt
+
+python search.py depth node_file.txt edge_file.txt heuristics_file.txt  A G output_depth.txt
+
+python search.py uniform node_file.txt edge_file.txt heuristics_file.txt  A G output_uniform.txt
+
+python search.py best node_file.txt edge_file.txt heuristics_file.txt  A G output_best.txt
+
+python search.py astar node_file.txt edge_file.txt heuristics_file.txt  A G output_astar.txt
